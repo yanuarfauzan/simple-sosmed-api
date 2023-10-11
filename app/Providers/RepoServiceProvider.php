ee402cea93f61bdea8b517e0daf671dc7538b044
yanuarfauzan <yanuarisnain017@gmail.com>
2023-10-11 09:23:58 +0700

    menambahkan bind repo dan service postingan

--- app/Providers/RepoServiceProvider.php
+++ app/Providers/RepoServiceProvider.php
@@ -6,11 +6,19 @@ use App\Repository\AuthRepo;
 use App\Service\AuthService;
 use App\Repository\ResetPwRepo;
 use App\Service\ResetPwService;
+use App\Repository\PenggunaRepo;
+use App\Service\PenggunaService;
+use App\Repository\PostinganRepo;
+use App\Service\PostinganService;
 use App\Service\ServAuthInterface;
 use App\Repository\RepoAuthInterface;
 use App\Service\ServResetPwInterface;
+use App\Service\ServPenggunaInterface;
+use App\Service\ServPostinganInterface;
 use Illuminate\Support\ServiceProvider;
 use App\Repository\RepoResetPwInterface;
+use App\Repository\RepoPenggunaInterface;
+use App\Repository\RepoPostinganInterface;
 
 class RepoServiceProvider extends ServiceProvider
 {
@@ -23,6 +31,10 @@ class RepoServiceProvider extends ServiceProvider
         $this->app->bind(ServAuthInterface::class, AuthService::class);
         $this->app->bind(RepoResetPwInterface::class, ResetPwRepo::class);
         $this->app->bind(ServResetPwInterface::class, ResetPwService::class);
+        $this->app->bind(RepoPenggunaInterface::class, PenggunaRepo::class);
+        $this->app->bind(ServPenggunaInterface::class, PenggunaService::class);
+        $this->app->bind(RepoPostinganInterface::class, PostinganRepo::class);
+        $this->app->bind(ServPostinganInterface::class, PostinganService::class);
     }
 
     /**
